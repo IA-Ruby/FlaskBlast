ea58e71377c30b38ffd8b1a89e810bea31034394
Francisco Eudes <eudesmendesfilho@gmail.com>
2024-07-22 20:49:03 -0300

    Changed movement to match isometric perspective

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,11 +2,20 @@ extends CharacterBody2D
 
 const SPEED = 10000.0
 
+@onready var sprite = $Sprite2D
+
 func _physics_process(delta):
 	var direction = Input.get_vector("left","right","up","down")
 	
+	if direction == Vector2(1,0):
+		sprite.flip_h = false
+	if direction == Vector2(-1,0):
+		sprite.flip_h = true
+		
 	if direction:
 		self.velocity = SPEED * direction * delta 
+		if direction.y != 0:
+			self.velocity *=  Vector2(1,0.5) 
 		if Input.is_action_pressed("sprint"):
 			self.velocity *= 2
 	else:
